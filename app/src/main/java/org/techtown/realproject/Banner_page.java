ad3942b06c16bc40019919793ca998da8d72f488
JSY27 <gimcher1234@naver.com>
2022-04-12 22:31:16 +0900

    배너페이지

---
+++ app/src/main/java/org/techtown/realproject/Banner_page.java
@@ -0,0 +1,13 @@
+package org.techtown.realproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Banner_page extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_banner);
+    }
+}
