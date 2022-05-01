fa660c711cc48cf3701fc8003d54e6d7f99314de
JSY27 <gimcher1234@naver.com>
2022-05-01 21:24:13 +0900

    배너

--- app/src/main/java/org/techtown/realproject/Banner_page.java
+++ app/src/main/java/org/techtown/realproject/Banner_page.java
@@ -3,6 +3,7 @@ package org.techtown.realproject;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.content.Intent;
 import android.database.Cursor;
@@ -28,70 +29,37 @@ import java.util.Date;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.w3c.dom.Text;
+
 import java.io.File;
 
 public class Banner_page extends AppCompatActivity {
-    ImageView imgVwSelected;
-    Button send_image, load_image;
-    File tempSelectFile;
+    Button btn_tv1, btn_tv2, btn_tv3;
+    ImageButton btn_tv4;
+    String keyword1, keyword2, keyword3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_banner);
 
-        send_image = findViewById(R.id.send_image);
-        send_image.setEnabled(false);
-        send_image.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                RequestHttpURLConnection.send2Server(tempSelectFile);
-            }
-        });
+        btn_tv1 = findViewById(R.id.btn_tv1);
+        btn_tv2 = findViewById(R.id.btn_tv2);
+        btn_tv3 = findViewById(R.id.btn_tv3);
+        ImageButton btn_tv4 = findViewById(R.id.btn_tv4);
+        /*
+        keyword1 =
+        keyword2 =
+        keyword3 =
+*/
+        btn_tv1.setEnabled(false);
+        btn_tv2.setEnabled(false);
+        btn_tv3.setEnabled(false);
+        btn_tv4.setEnabled(false);
 
-        load_image = findViewById(R.id.load_image);
-        load_image.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(intent, 1);
-            }
-        });
-
-        imgVwSelected = findViewById(R.id.imgVwSelected);
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode != 1 || resultCode != RESULT_OK) {
-            return;
-        }
-
-        Uri dataUri = data.getData();
-        imgVwSelected.setImageURI(dataUri);
-
-        try {
-            InputStream in = getContentResolver().openInputStream(dataUri);
-            Bitmap image = BitmapFactory.decodeStream(in);
-            imgVwSelected.setImageBitmap(image);
-            in.close();
-
-            String date = new SimpleDateFormat("yyyy_MM_dd_hh_mm_ss").format(new Date());
-            //tempSelectFile.createNewFile();
-            String StoragePath = Environment.getExternalStorageDirectory().getCanonicalPath();
-            String savePath = StoragePath + "/Pictures/Test";
-            File f = new File(savePath);
-            if (!f.isDirectory())
-                f.mkdirs();
-            tempSelectFile = new File(savePath + "/temp_" + date + ".jpeg");
-            FileOutputStream out = new FileOutputStream(tempSelectFile);
-            image.compress(Bitmap.CompressFormat.JPEG, 100, out);
-            out.close();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-        send_image.setEnabled(true);
+        btn_tv1.setVisibility(View.INVISIBLE);
+        btn_tv2.setVisibility(View.INVISIBLE);
+        btn_tv3.setVisibility(View.INVISIBLE);
+        btn_tv4.setVisibility(View.INVISIBLE);
     }
     public void backspaceClicked(View v){
         Intent intent = new Intent(getApplicationContext(),Select_page.class);
@@ -102,5 +70,32 @@ public class Banner_page extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(),Select_page.class);
         startActivity(intent);
         finish();
+    }
+    public void btn_tv1Clicked(View v){
+       /* if(){
+            btn_tv1.setVisibility(View.VISIBLE);
+            btn_tv1.setEnabled(true);
+        }*/
+    }
+    public void btn_tv2Clicked(View v){
+        /*if(){
+            btn_tv2.setVisibility(View.VISIBLE);
+            btn_tv2.setEnabled(true);
+        }*/
+    }
+    public void btn_tv3Clicked(View v){
+        /*if(){
+            btn_tv3.setVisibility(View.VISIBLE);
+            btn_tv3.setEnabled(true);
+        }*/
+    }
+    public void btn_tv4Clicked(View v){
+        if(btn_tv1.isEnabled() == true) {
+            btn_tv4.setVisibility(View.VISIBLE);
+            btn_tv4.setEnabled(true);
+        }
+            Intent intent = new Intent(getApplicationContext(), List_page.class);
+            startActivity(intent);
+            finish();
     }
 }
